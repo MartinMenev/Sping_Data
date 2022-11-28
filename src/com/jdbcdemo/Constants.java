6c495139a7caa13a1922d27cffdd660c8b6da0dc
MartinMenev <martin.menew@gmail.com>
2022-11-28 20:18:40 +0200

    New commit

---
+++ src/com/jdbcdemo/Constants.java
@@ -0,0 +1,11 @@
+package com.jdbcdemo;
+
+enum Constants {
+    ;
+    static final String USER_KEY = "user";
+    static final String USER_VALUE = "root";
+    static final String PASSWORD_KEY = "password";
+    static final String PASSWORD_VALUE = "roni95";
+
+    static final String JDBC_URL = "jdbc:mysql://localhost:3306/minions_db";
+}
