e64481846c24ee20c6de47470d2a7d30c687779a
MartinMenev <martin.menew@gmail.com>
2022-11-28 20:25:38 +0200

    New commit

--- src/com/jdbcdemo/GetVillainsNames.java
+++ src/com/jdbcdemo/GetVillainsNames.java
@@ -5,13 +5,13 @@ import java.sql.*;
 
 public class GetVillainsNames {
     private static final String GET_VILLAIN_NAMES = "SELECT \n" +
-                    "    v.`name`, COUNT(DISTINCT m.`name`) AS number\n" +
+                    "v.`name`, COUNT(DISTINCT m.`name`) AS number\n" +
                     "FROM\n" +
-                    "    `villains` AS v\n" +
-                    "        JOIN\n" +
-                    "    `minions_villains` AS mv ON v.`id` = mv.`villain_id`\n" +
-                    "        JOIN\n" +
-                    "    `minions` AS m ON mv.`minion_id` = m.`id`\n" +
+                    "`villains` AS v\n" +
+                    "JOIN\n" +
+                    "`minions_villains` AS mv ON v.`id` = mv.`villain_id`\n" +
+                    "JOIN\n" +
+                    "`minions` AS m ON mv.`minion_id` = m.`id`\n" +
                     "GROUP BY v.`name`\n" +
                     "HAVING number > 15\n" +
                     "ORDER BY number DESC;";
